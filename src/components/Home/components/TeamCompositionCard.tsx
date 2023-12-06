1aeb559a6ca59f6c0b638b01cf4892a0cd768a87
smurawczik <sebastian.murawczik@gmail.com>
2023-12-06 21:03:06 +0100

    user hook fake, and adding loading based state to card

--- src/components/Home/components/TeamCompositionCard.tsx
+++ src/components/Home/components/TeamCompositionCard.tsx
@@ -3,25 +3,37 @@ import { FC } from "react";
 import { useUser } from "../hooks/useUser";
 
 export const TeamCompositionCard: FC<{ userId: string }> = ({ userId }) => {
-  useUser(userId);
+  const { loading } = useUser(userId);
 
   return (
-    <Card isFooterBlurred radius="lg" className="border-none">
+    <Card
+      isFooterBlurred
+      radius="lg"
+      className="border-none bg-gradient-to-b from-indigo-500 via-purple-500"
+    >
+      {loading && (
+        <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
+          <div className="w-4 h-4 border-2 border-white rounded-full animate-spin" />
+        </div>
+      )}
       <Image
         alt="Woman listing to music"
         className="object-cover"
-        height={200}
+        height={250}
         src="/images/hero-card.jpeg"
-        width={200}
+        width={250}
       />
-      <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-        <p className="text-tiny text-white/80">Available soon.</p>
+      <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10 ">
+        <p className="text-tiny text-white/80">
+          {loading ? "loading..." : "Available soon"}
+        </p>
         <Button
-          className="text-tiny text-white bg-black/20"
+          className="text-tiny text-white bg-black/20 disabled:bg-black/50"
           variant="flat"
           color="default"
           radius="lg"
           size="sm"
+          disabled={loading}
         >
           Notify me
         </Button>
--- src/components/Home/hooks/useUser.ts
+++ src/components/Home/hooks/useUser.ts
@@ -1,5 +1,20 @@
+import { useEffect, useRef, useState } from "react";
+
 export const useUser = (userId: string) => {
+  const userIdRef = useRef(userId);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (userIdRef.current !== userId) {
+        return;
+      }
+
+      setLoading(false);
+    }, Math.random() * 5000);
+  }, [userId]);
+
   return {
-    id: userId,
+    loading,
   };
 };
